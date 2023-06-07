058bc6da6f8012c916a89c86086765f6f1ca9b4f
YoCazza <101638151+YoCazza@users.noreply.github.com>
2023-06-07 10:32:36 +0200

    Make config modern

--- config.lua
+++ config.lua
@@ -1,12 +1,20 @@
 CH = {}
 
--- This will be the ACE Perms name for your group(s).
+--░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░██╗░░░░░
+--██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗██║░░░░░
+--██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║██║░░░░░
+--██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║██║░░░░░
+--╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║███████╗
+--░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝
 CH.AcePermsName = "CH.Kickgun"
-
--- This will be your in-game slash command
 CH.KickgunCommand = "kickgun"
 
--- Language options:
+--██╗░░░░░░█████╗░███╗░░██╗░██████╗░██╗░░░██╗░█████╗░░██████╗░███████╗
+--██║░░░░░██╔══██╗████╗░██║██╔════╝░██║░░░██║██╔══██╗██╔════╝░██╔════╝
+--██║░░░░░███████║██╔██╗██║██║░░██╗░██║░░░██║███████║██║░░██╗░█████╗░░
+--██║░░░░░██╔══██║██║╚████║██║░░╚██╗██║░░░██║██╔══██║██║░░╚██╗██╔══╝░░
+--███████╗██║░░██║██║░╚███║╚██████╔╝╚██████╔╝██║░░██║╚██████╔╝███████╗
+--╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░░╚═════╝░╚═╝░░╚═╝░╚═════╝░╚══════╝
 CH.KickMessage = "⚪ You are kicked from"
 CH.ServerName = "SERVERNAME"
 CH.AdminNameOnKick = "⚪ Admin:"
@@ -21,6 +29,12 @@ CH.KickGunDelayCantKick2 = "seconds~s~ to kick a player again"
 CH.KickgunWarningMessage = "The Kickgun is ~g~active!~s~~n~Reason:"
 CH.KickGunInfoboxIcon = "~BLIP_INFO_ICON~" -- All the icons / inputs: https://pastebin.com/nqNYWMSB
 
+--██████╗░███████╗██╗░░░░░░█████╗░██╗░░░██╗
+--██╔══██╗██╔════╝██║░░░░░██╔══██╗╚██╗░██╔╝
+--██║░░██║█████╗░░██║░░░░░███████║░╚████╔╝░
+--██║░░██║██╔══╝░░██║░░░░░██╔══██║░░╚██╔╝░░
+--██████╔╝███████╗███████╗██║░░██║░░░██║░░░
+--╚═════╝░╚══════╝╚══════╝╚═╝░░╚═╝░░░╚═╝░░░
 -- You can set a delay when a staff member kicks a player, this delay starts as soon as you have kicked a player and wil display this in the top-left infobox.
 CH.KickgunDelay = true
 CH.KickgunDelaySeconds = 10
