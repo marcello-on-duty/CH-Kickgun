958cbff4e097899e4be368260ffa6aedc323c3db
YoCazza <101638151+YoCazza@users.noreply.github.com>
2023-08-03 00:03:32 +0200

    Optimize client code

--- cl_kickgun.lua
+++ cl_kickgun.lua
@@ -55,40 +55,34 @@ end
 
 RegisterCommand(CH.KickgunCommand, function(source, args, rawCommand)
     checkRole()
-    if allowed == true then
-
-      for i,theArg in pairs(args) do
-         if i ~= 0 then
-            reden = reden.." "..theArg
+    if allowed then
+        for i, theArg in pairs(args) do
+            if i ~= 0 then
+                reden = reden .. " " .. theArg
+            end
         end
-    end
-    if chkickgun == false then
-        chkickgun = true
-        sendM(CH.KickgunActiveMessage)
-        Citizen.Wait(1)
+        chkickgun = not chkickgun
+        sendM(chkickgun and CH.KickgunActiveMessage or CH.KickGunInactiveMessage)
+        reden = not chkickgun and "" or reden
     else
-        chkickgun = false
-        sendM(CH.KickGunInactiveMessage)
-        reden = ""
+        sendM(CH.NoAccesToKickgun)
     end
-else
-    sendM(CH.NoAccesToKickgun)
-end
 end, false)
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        if chkickgun and not CH.KickgunDelay then
-            InfoBox(CH.KickGunInfoboxIcon .."~s~ ".. CH.KickgunWarningMessage .."".. reden)
-        elseif chkickgun and CH.KickgunDelay then
-            if CHrunning then
-                InfoBox(CH.KickGunInfoboxIcon .."~s~ ".. CH.KickgunWarningMessage .."".. reden .."~n~".. CH.KickGunDelayMessage .." ".. CHDelay) 
-            else
-                InfoBox(CH.KickGunInfoboxIcon .."~s~ ".. CH.KickgunWarningMessage .."".. reden .."~n~".. CH.KickGunDelayMessage .." ".. CH.KickgunDelayReady) 
+        if chkickgun then
+            local message = CH.KickgunWarningMessage .. reden
+            if CH.KickgunDelay then
+                message = message .. "~n~" .. CH.KickGunDelayMessage
+                if CHrunning then
+                    message = message .. " " .. CHDelay
+                else
+                    message = message .. " " .. CH.KickgunDelayReady
+                end
             end
-        elseif not chkickgun then
-            Citizen.Wait(200)
+            InfoBox(CH.KickGunInfoboxIcon .. "~s~ " .. message)
         end
     end
 end)
@@ -96,29 +90,25 @@ end)
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(1)
-        if chkickgun then
-            if IsPlayerFreeAiming(PlayerId()) then
-                local entity = getEntity(PlayerId())
-                local entityowner = NetworkGetEntityOwner(entity)
-                local ownerid = GetPlayerServerId(entityowner)
-                local ownername = GetPlayerName(entityowner)
-                local kickREDEN = reden
-                if GetEntityType(entity) == 1 then
-                 if IsPedShooting(PlayerPedId()) then
-                   if CH.KickgunDelay and CHDelay == 0 then
-                      TriggerServerEvent("ch_kickgun:kick", ownerid, kickREDEN)
-                      CHDelay = CH.KickgunDelaySeconds
-                      StartCountDown()
-                  elseif not CH.KickgunDelay then
-                   TriggerServerEvent("ch_kickgun:kick", ownerid, kickREDEN)
-               else
-                sendM(CH.KickGunDelayCantKick1 .." "..CHDelay.." ".. CH.KickGunDelayCantKick2)
+        if chkickgun and IsPlayerFreeAiming(PlayerId()) then
+            local entity = getEntity(PlayerId())
+            local entityowner = NetworkGetEntityOwner(entity)
+            local ownerid = GetPlayerServerId(entityowner)
+            local ownername = GetPlayerName(entityowner)
+            local kickREDEN = reden
+            if GetEntityType(entity) == 1 and IsPedShooting(PlayerPedId()) then
+                if CH.KickgunDelay and CHDelay == 0 then
+                    CHDelay = CH.KickgunDelaySeconds
+                    StartCountDown()
+                     TriggerServerEvent("ch_kickgun:kick", ownerid, kickREDEN)
+                elseif not CH.KickgunDelay then
+                     TriggerServerEvent("ch_kickgun:kick", ownerid, kickREDEN)
+                else
+                    sendM(CH.KickGunDelayCantKick1 .." "..CHDelay.." ".. CH.KickGunDelayCantKick2)
+                end
             end
+        elseif not chkickgun then
+            Citizen.Wait(200)
         end
     end
-end
-elseif not chkickgun then
- Citizen.Wait(200)
-end
-end
 end)
