60c9226dbb53cfd0137852974313f61262ef06d8
Marcello <88586178+marcello-on-duty@users.noreply.github.com>
2023-08-03 16:49:28 +0200

    Update sv_kickgun.lua

--- sv_kickgun.lua
+++ sv_kickgun.lua
@@ -1,18 +1,14 @@
 RegisterServerEvent("ch_kickgun:checkRole")
 AddEventHandler("ch_kickgun:checkRole", function()
-    if IsPlayerAceAllowed(source, CH.AcePermsName) then
-        TriggerClientEvent("ch_kickgun:returnCheck", source, true)
-    else
-        TriggerClientEvent("ch_kickgun:returnCheck", source, false)
-    end
+    if not IsPlayerAceAllowed(source, CH.AcePermsName) then return TriggerClientEvent("ch_kickgun:returnCheck", source, false) end
+
+    TriggerClientEvent("ch_kickgun:returnCheck", source, true)
 end)
 
 RegisterServerEvent("ch_kickgun:kick")
 AddEventHandler("ch_kickgun:kick", function(kickID, kickREDEN)
-    if IsPlayerAceAllowed(source, CH.AcePermsName) then
-        local tijd = os.date("%d/%m/%Y %X")
-        DropPlayer(kickID, CH.KickMessage..' '.. CH.ServerName ..'\n'.. CH.AdminNameOnKick ..' '..GetPlayerName(source)..'\n '.. CH.KickReason ..' '..kickREDEN..'\n' .. CH.KickDate ..' '.. tijd)
-    else
-        print("[^1WARNING^0] "..GetPlayerName(source).." tried to trigger the kickgun but has no permissions.")
-    end
+    if not IsPlayerAceAllowed(source, CH.AcePermsName) then return print("[^1WARNING^0] "..GetPlayerName(source).." tried to trigger the kickgun but has no permissions.") end
+        
+    local tijd = os.date("%d/%m/%Y %X")
+    DropPlayer(kickID, CH.KickMessage..' '.. CH.ServerName ..'\n'.. CH.AdminNameOnKick ..' '..GetPlayerName(source)..'\n '.. CH.KickReason ..' '..kickREDEN..'\n' .. CH.KickDate ..' '.. tijd)
 end)
